ebff1f60e7858b443985f61311f7d2cd5d37ec4c
xcoudzy <amit4.pagal@gmail.com>
2024-06-29 14:56:28 +0530

    200

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //REGISTER
-router.post("/register", async (req, res) => {
+router.post("/auth/register", async (req, res) => {
   try {
     //generate new password
     const salt = await bcrypt.genSalt(10);
@@ -20,22 +20,25 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 //LOGIN
-router.post("/login", async (req, res) => {
+router.post("/auth/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(404).json("user not found");
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
 
-    res.status(200).json(user)
+    res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
