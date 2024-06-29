bb8df7de59b10f390fbe149c4f4632ef0e6aa68a
xcoudzy <amit4.pagal@gmail.com>
2024-06-29 14:47:00 +0530

    200

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,75 @@
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { Nav, NavDropdown } from "react-bootstrap";
+import { useHistory } from "react-router";
+
+export default function Topbar() {
+  const { user, setAuth } = useContext(AuthContext);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const history = useHistory();
+
+  const logout = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    setAuth(null);
+    history.push("/login");
+  };
+
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">CloudSocial</span>
+        </Link>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+
+        <Nav>
+          <NavDropdown.Item onClick={logout}>Log Out</NavDropdown.Item>
+        </Nav>
+
+        <Link to={`/profile/${user.username}`}>
+          <img
+            src={
+              user.profilePicture
+                ? PF + user.profilePicture
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+            className="topbarImg"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
