8e63ef625f6e01df86e0ba02ee3b1f96a64b4536
xcoudzy <amit4.pagal@gmail.com>
2024-07-15 19:11:39 +0530

    vercel fix

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -30,7 +30,7 @@ export default function Register() {
         );
         history.push("/login");
       } catch (err) {
-        console.log(err);
+        console.log(err.message);
       }
     }
   };
